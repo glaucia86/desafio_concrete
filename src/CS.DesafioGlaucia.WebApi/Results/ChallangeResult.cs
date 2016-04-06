4508261afd8dc07b56739c9da8cab4c99f2848cd
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-04-06 11:14:17 -0300

    Identacao do código do arquivo ChallengeResult.cs através do Stylecop

--- src/CS.DesafioGlaucia.WebApi/Results/ChallangeResult.cs
+++ src/CS.DesafioGlaucia.WebApi/Results/ChallangeResult.cs
@@ -12,11 +12,13 @@ namespace CS.DesafioGlaucia.WebApi.Results
 
         public HttpRequestMessage Request { get; set; }
 
+
         public ChallangeResult(string loginProvider, ApiController controller)
         {
             LoginProvider = loginProvider;
             Request = controller.Request;
         }
+
 
         public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
         {
@@ -24,6 +26,7 @@ namespace CS.DesafioGlaucia.WebApi.Results
 
             var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
             response.RequestMessage = Request;
+
             return Task.FromResult(response);
         }
     }
